00e4395dd239d8b2218ad87280eaa8519628b4aa
ajeevsan <sanjeevmajhi036@gmail.com>
2025-09-17 13:46:12 +0530

    first commit

--- gateway.js
+++ gateway.js
@@ -1,18 +1,19 @@
+require('dotenv').config()
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
-
+const target = process.env.BACKEND_URL
 // Proxy /api/upload → backend /upload
 app.use('/api/upload', createProxyMiddleware({
-  target: 'http://localhost:3000',
+  target: target,
   changeOrigin: true,
   pathRewrite: { '^/api': '' }, // remove /api prefix
 }));
 
 // Proxy /api/download → backend /download
 app.use('/api/download', createProxyMiddleware({
-  target: 'http://localhost:3000',
+  target: target,
   changeOrigin: true,
   pathRewrite: { '^/api': '' },
 }));
